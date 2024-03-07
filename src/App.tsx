b34d844adb2b367905e747b6eca8f6546feaaec9
OneDeadL1ne <119927472+OneDeadL1ne@users.noreply.github.com>
2024-03-07 17:23:14 +0300

    add burder

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
 export default function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />} />
+		</Routes>
+	);
 }
---
+++ src/components/Burger.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { BurgerProps } from "./Layout";
+import { useState } from "react";
+import BurgerIcon from "@/assets/images/burger.svg?react";
+import Logo from "@/assets/icons/logo.svg?react";
+
+export function Burger({
+	links: { aboutRef, contactsRef, portfolioRef, serviceRef },
+}: {
+	links: BurgerProps;
+}) {
+	const [open, setOpen] = useState(false);
+	return (
+		<Sheet onOpenChange={setOpen} open={open}>
+			<SheetTrigger asChild>
+				<BurgerIcon height={40} />
+			</SheetTrigger>
+			<SheetContent side="left" className="bg-[#36353A]">
+				<SheetHeader>
+					<SheetTitle>
+						<a className=" text-3xl font-bold mr-5">ADSSCODE</a>
+					</SheetTitle>
+				</SheetHeader>
+				<div className="grid grid-rows-4 ">
+					<Button
+						className=""
+						onClick={() => {
+							aboutRef.current?.scrollIntoView({
+								behavior: "smooth",
+							});
+							setOpen(false);
+						}}
+					>
+						О нас
+					</Button>
+					<Button
+						className=""
+						onClick={() => {
+							serviceRef.current?.scrollIntoView({
+								behavior: "smooth",
+							});
+							setOpen(false);
+						}}
+					>
+						Наши услуги
+					</Button>
+					<Button
+						className=""
+						onClick={() => {
+							portfolioRef.current?.scrollIntoView({
+								behavior: "smooth",
+							});
+							setOpen(false);
+						}}
+					>
+						Портфолио
+					</Button>
+					<Button
+						className=""
+						onClick={() => {
+							contactsRef.current?.scrollIntoView({
+								behavior: "smooth",
+							});
+							setOpen(false);
+						}}
+					>
+						Контакты
+					</Button>
+				</div>
+			</SheetContent>
+		</Sheet>
+	);
+}
